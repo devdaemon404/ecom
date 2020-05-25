c452b4fd85c7a15a7de8c712a794cb89f7d6c89e
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-25 12:59:13 +0530

    broken our homepage into smaller items and added styles to each broken component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Homepage';
+import Homepage from './pages/homepage/homepage.componenet';
 function App() {
   return (
     <div>
