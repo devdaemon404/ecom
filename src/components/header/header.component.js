8e52c52076b019ad842b3dd84e88d05838e85d6e
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-27 15:52:58 +0530

    implemented cartReducer to toggle cart-dropdown component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './header.styles.scss';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import { auth } from '../../firebase/firebase.utils';
-const Header = ({ currentUser }) => {
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import './header.styles.scss';
+
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -26,14 +32,17 @@ const Header = ({ currentUser }) => {
                             SIGN IN
                         </Link>
                 }
-
+                <CartIcon />
             </div>
+            {!hidden &&
+                <CartDropdown />}
         </div>
     )
 }
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
