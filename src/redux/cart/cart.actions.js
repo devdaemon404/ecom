7b09d1dcf5dfb4b38b5d4f7780dac1b50bfe7aac
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-28 10:49:54 +0530

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,9 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './cart.types';
+import {
+    TOGGLE_CART_HIDDEN,
+    ADD_ITEM,
+    CLEAR_ITEM_FROM_CART,
+    REMOVE_ITEM
+} from './cart.types';
 
 export const toggleCartHidden = () => ({
     type: TOGGLE_CART_HIDDEN
@@ -8,3 +13,13 @@ export const addItem = (item) => ({
     type: ADD_ITEM,
     payload: item
 });
+
+export const clearItemFromCart = item => ({
+    type: CLEAR_ITEM_FROM_CART,
+    payload: item
+});
+
+export const removeItem = item => ({
+    type: REMOVE_ITEM,
+    payload: item
+})
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,10 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import {
+    TOGGLE_CART_HIDDEN,
+    ADD_ITEM,
+    CLEAR_ITEM_FROM_CART,
+    REMOVE_ITEM
+} from './cart.types';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,7 +22,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case ADD_ITEM:
             return {
                 ...state,
-                cartItems: addItemToCart(state.cartItems,payload)
+                cartItems: addItemToCart(state.cartItems, payload)
+            }
+        case REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, payload)
+            }
+        case CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== payload.id)
             }
         default:
             return state;
