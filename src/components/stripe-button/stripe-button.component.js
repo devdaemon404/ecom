d1133ec689c22d37419f0dd3edad4a6f6fc576f0
Bharath Karanth <bharathkaranth07@gmail.com>
2020-05-31 15:53:35 +0530

    Added stripe payment integration to our app

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = process.env.REACT_APP_STRIPE_API_KEY;
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+  return (
+    <StripeCheckout
+      label='Pay now'
+      name='ecom corp'
+      billingAddress
+      shippingAddress
+      image=''
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
