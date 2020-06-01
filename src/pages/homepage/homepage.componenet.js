c9372309e1f9560fb80b02af7c7c2b06aaa71861
Bharath Karanth <bharathkaranth07@gmail.com>
2020-06-01 09:10:08 +0530

    Remove scss files and added css in js using styled-component lib

--- src/pages/homepage/homepage.componenet.js
+++ src/pages/homepage/homepage.componenet.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import './homepage.styles.scss';
+import React from 'react';
+
 import Directory from '../../components/directory/directory.component';
+
+import { HomePageContainer } from './homepage.styles';
+
 const Homepage = () => {
-    return (
-        <div className='homepage'>
-            <Directory />
-        </div>
-    )
-}
+  return (
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer>
+  );
+};
 
 export default Homepage;
